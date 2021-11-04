9d3e320ad52bbe450c732b16eb5355860840add1
danielchwi <daniel.widodo@bitcoin.co.id>
2021-11-04 13:47:11 +0700

    Create folder/package for controller, databse, & routes

--- go-admin/main.go
+++ go-admin/main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"govue/database"
+	"govue/routes"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	db, err := gorm.Open(mysql.Open("root:@/govue"), &gorm.Config{})
-	if err != nil {
-		panic("Could not connect to databases")
-	}
-	fmt.Println(db)
-
+	database.Connect()
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	routes.Setup(app)
 
 	app.Listen(":3000")
 }
