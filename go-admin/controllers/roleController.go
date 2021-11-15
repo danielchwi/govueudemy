da7c7a882a22376f0be7bc8dbdf119ec976eb657
danielchwi <daniel.widodo@bitcoin.co.id>
2021-11-16 00:47:55 +0700

    many2many relation CRUD

--- go-admin/controllers/roleController.go
+++ go-admin/controllers/roleController.go
@@ -17,10 +17,35 @@ func GetRoles(c *fiber.Ctx) error {
 }
 
 func CreateRole(c *fiber.Ctx) error {
-	var role models.Role
+	var roleDto fiber.Map
 
-	if err := c.BodyParser(&role); err != nil {
+	if err := c.BodyParser(&roleDto); err != nil {
 		return err
+	}
+
+	list := roleDto["permissions"].([]interface{})
+
+	permissions := make([]*models.Permission, len(list))
+
+	for i, permissionId := range list {
+		var permissionIdFloat float64
+		s, ok := permissionId.(string)
+		if ok {
+			permissionIdFloat, _ = strconv.ParseFloat(s, 64)
+		}
+		f, ok := permissionId.(float64)
+		if ok {
+			permissionIdFloat = f
+		}
+
+		permissions[i] = &models.Permission{
+			Id: uint(permissionIdFloat),
+		}
+	}
+
+	role := models.Role{
+		Name:        roleDto["name"].(string),
+		Permissions: permissions,
 	}
 
 	database.DB.Create(&role)
@@ -33,7 +58,7 @@ func GetRole(c *fiber.Ctx) error {
 
 	role := models.Role{Id: uint(id)}
 
-	database.DB.Find(&role)
+	database.DB.Preload("Permissions").Find(&role)
 
 	return c.JSON(role)
 }
@@ -41,14 +66,41 @@ func GetRole(c *fiber.Ctx) error {
 func UpdateRole(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 
-	role := models.Role{Id: uint(id)}
+	var roleDto fiber.Map
 
-	database.DB.Find(&role)
-
-	if err := c.BodyParser(&role); err != nil {
+	if err := c.BodyParser(&roleDto); err != nil {
 		return err
 	}
 
+	list := roleDto["permissions"].([]interface{})
+
+	//return c.JSON(list)
+
+	permissions := make([]*models.Permission, len(list))
+
+	for i, permissionId := range list {
+		var permissionIdFloat float64
+		s, ok := permissionId.(string)
+		if ok {
+			permissionIdFloat, _ = strconv.ParseFloat(s, 64)
+		}
+		f, ok := permissionId.(float64)
+		if ok {
+			permissionIdFloat = f
+		}
+
+		permissions[i] = &models.Permission{
+			Id: uint(permissionIdFloat),
+		}
+	}
+
+	role := models.Role{
+		Id:          uint(id),
+		Name:        roleDto["name"].(string),
+		Permissions: permissions,
+	}
+
+	database.DB.Model(&role).Association("Permissions").Replace(role.Permissions)
 	database.DB.Updates(&role)
 
 	return c.JSON(role)
@@ -59,6 +111,7 @@ func DeleteRole(c *fiber.Ctx) error {
 
 	role := models.Role{Id: uint(id)}
 
+	database.DB.Model(&role).Association("Permissions").Clear()
 	database.DB.Delete(&role)
 
 	return nil
